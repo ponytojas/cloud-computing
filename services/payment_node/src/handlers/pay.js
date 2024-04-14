3665303ae275815cff196429a7902b155e3cebd8
Daniel Villalobos del Baño <daniel.villalobosdel@icloud.com>
2024-04-14 18:19:33 +0200

    Added payment node

---
+++ services/payment_node/src/handlers/pay.js
@@ -0,0 +1,38 @@
+import { Redis } from "../utils/redis.js";
+
+export const handlePay = async (req, res) => {
+  const client = new Redis();
+  const { userId } = req.params;
+
+  // Delete the cart from the key `cart-user-userId`
+  const cartKey = `cart-user-${userId}`;
+  await client.del(cartKey);
+
+  res.status(200).json({ message: "Payment successful" });
+};
+
+export const handleTotal = async (req, res) => {
+  const client = new Redis();
+  const { userId } = req.params;
+
+  // Get the cart from the key `cart-user-userId`
+  const cartKey = `cart-user-${userId}`;
+  const cart = JSON.parse(await client.get(cartKey));
+
+  if (!cart) {
+    return res.status(200).json({});
+  }
+  const total = await Promise.all(
+    Object.entries(cart).map(async ([productId, quantity]) => {
+      const response = await fetch(
+        `${process.env.STORE_SERVICE_URL}/product/${productId}`
+      );
+      const product = await response.json();
+      const { pricing } = product[0];
+      return pricing * quantity;
+    })
+  );
+
+  const grandTotal = total.reduce((acc, curr) => acc + curr, 0);
+  res.status(200).json({ total: grandTotal });
+};
