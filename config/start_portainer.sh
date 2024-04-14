da4938fed683d1c004cd2567bb26aa2a4b7aaa99
Daniel Villalobos del Baño <daniel.villalobosdel@icloud.com>
2024-04-14 11:58:47 +0200

    Correct port

--- config/start_portainer.sh
+++ config/start_portainer.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-docker run -d -p 9001:9001 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/portainer-ce:2.19.4
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
